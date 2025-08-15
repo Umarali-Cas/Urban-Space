4076ccbf6e5d135e79d0e41db178e3dd7ae42a9c
Umarali <umaraliomorov13@gmail.com>
2025-08-15 22:39:51 +0600

    fix: fixed adaptation problems

--- src/widgets/Articles/ui/Articles.tsx
+++ src/widgets/Articles/ui/Articles.tsx
@@ -1,8 +1,29 @@
+'use client'
+
 import { ArticlesCard } from '@/entities/ArticlesCard'
 import classes from './Articles.module.scss'
 import image from '../assets/Image-1.png'
+import { useEffect, useState } from 'react'
 
 export function Articles() {
+  const [isHide, setIsHide] = useState(false)
+
+  useEffect(() => { //Адаптация под мобильные устройства, временно пока нет бекенда
+    const handleResize = () => {
+      if (window.innerWidth <= 1200) {
+        setIsHide(true);
+      } else {
+        setIsHide(false);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  
   return (
     <section className={classes.articles}>
       <div className={classes.articles__wrapper}>
@@ -19,20 +40,25 @@ export function Articles() {
               userName="Wanda Wingleton"
               avatarUrl={''}
             />
-            <ArticlesCard
-              article="Your expectations will fly sky high. I felt like I was soaring."
-              articleName="Название статьи"
-              role="Публицист"
-              userName="Wanda Wingleton"
-              avatarUrl={image.src}
-            />
-            <ArticlesCard
-              article="Your expectations will fly sky high. I felt like I was soaring."
-              articleName="Название статьи"
-              role="Публицист"
-              userName="Wanda Wingleton"
-              avatarUrl={''}
-            />
+            {!isHide && (
+              <>
+              <ArticlesCard
+                article="Your expectations will fly sky high. I felt like I was soaring."
+                articleName="Название статьи"
+                role="Публицист"
+                userName="Wanda Wingleton"
+                avatarUrl={image.src}
+              />
+
+              <ArticlesCard
+                article="Your expectations will fly sky high. I felt like I was soaring."
+                articleName="Название статьи"
+                role="Публицист"
+                userName="Wanda Wingleton"
+                avatarUrl={image.src}
+                />
+                </>
+            )}
           </div>
         </div>
       </div>
