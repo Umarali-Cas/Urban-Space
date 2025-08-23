99e969446cad18e67302a84ad6a300f5e7c4e097
Umarali Omorov <umaraliomorov13@gmail.com>
2025-08-24 02:10:30 +0600

    Revert "feat: map"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { EcoBaner } from "@/entities/EcoBaner";
-import { MapSearch } from "@/widgets/MapSearch";
-import { Articles } from "@/widgets/Articles";
-import { Hero } from "@/widgets/Hero";
-import { LastIdeas } from "@/widgets/LastIdeas";
-import { News } from "@/widgets/News";
-
+import { EcoBaner } from '@/entities/EcoBaner'
+import { Articles } from '@/widgets/Articles'
+import { Hero } from '@/widgets/Hero'
+import { LastIdeas } from '@/widgets/LastIdeas'
+import { News } from '@/widgets/News'
 
 export default function Home() {
   return (
@@ -16,7 +14,6 @@ export default function Home() {
         subtitle="Свежие предложения от участников сообщества"
         viewCards={3}
       />
-      <MapSearch />
       <News />
       <Articles />
     </>
