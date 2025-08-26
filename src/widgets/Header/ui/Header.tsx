b1d7053638bdf154f8efe45e168c906974eafd42
Umarali <umaraliomorov13@gmail.com>
2025-08-26 14:17:22 +0600

    feat: added burger menu

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -2,13 +2,19 @@ import { Logo } from '@/entities/Logo'
 import classes from './Header.module.scss'
 import { NavBar } from '@/features/NavBar'
 import { Button } from '@/shared/Button'
+import { BurgerMenu } from '@/entities/BurgerMenu'
 
 export function Header() {
+
+
   return (
     <header className={classes.header}>
         <Logo />
         <NavBar />
-        <Button text='Войти'/>
+        <div className={classes.header__actions}>
+          <Button text='Войти'/>
+          <BurgerMenu />
+        </div>
     </header>
   )
 }
