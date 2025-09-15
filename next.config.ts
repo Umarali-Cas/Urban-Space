275958814db20da01f6f6023b8c66eeab89777ba
Umarali <umaraliomorov13@gmail.com>
2025-09-15 16:03:44 +0600

    fix: maybe fixed

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     // добавляй сюда все домены, с которых будут подгружаться картинки
-    domains: ['example.com', 'cdn.yourapp.com'],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8000/:path*',
-      },
-    ]
+    domains: ['example.com', 'cdn.yourapp.com', 'api.urbanspace.sdinis.org'],
   },
   // Рекомендуется для продакшна
   output: 'standalone',
